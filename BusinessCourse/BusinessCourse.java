03646d846e2d35df99af0243aaf1f3b77094ffd5
Forzadani <99603100+Forzadani@users.noreply.github.com>
2022-02-13 16:51:55 +0100

    Update BusinessCourse.java

--- BusinessCourse/BusinessCourse.java
+++ BusinessCourse/BusinessCourse.java
@@ -20,7 +20,7 @@ public class BusinessCourse extends Course{
     
     /**
      * This is the constructor with parameters.
-     * @param name is the name of the Business Course.
+     * @param name is the name of the course.
      * @param teacher is the name of the teacher.
      * @param id is the identification of the course.
      * @param duration is the duration of the course.
@@ -42,6 +42,79 @@ public class BusinessCourse extends Course{
         copy.teacher = this.teacher;
         copy.id = this.id;
         copy.duration = this.duration;
+    }
+    
+    /**
+     * This method set the name of course.
+     * @param name is the name of the Course.
+     */
+    public void setName(String name){
+        if(name.length()>=4){
+            this.name = name;
+        }
+    }
+    
+    /**
+     * This method set the name of the teacher.
+     * @param teacher is the name of the Teacher.
+     */
+    public void setTeacher(String teacher){
+        String nombre = teacher.substring(teacher.indexOf(",")+1);
+        if(teacher.contentEquals(",") && !nombre.contentEquals(" ")){
+            this.teacher = teacher;
+        }
+    }
+    
+    /**
+     * This method set the ID of the course.
+     * @param id  is the ID of the Course.
+     */
+    public void setId(String id){
+        if(id.startsWith(name.substring(0, 1)) && id.endsWith(teacher.substring(0, 1))){
+            this.id = id;
+        }
+    }
+    
+    /**
+     * This method set the duration of the course.
+     * @param duration is the duration of the Course.
+     */
+    public void setDuration(int duration){
+        if(duration >= 30){
+            this.duration = duration;
+        }
+    }
+    
+    /**
+     * This method return the value of the name of course.
+     * @return a String.
+     */
+    public String getName(){
+        return this.name;
+    }
+    
+    /**
+     * This method return the teacher's name.
+     * @return a String.
+     */
+    public String getTeacher(){
+        return this.teacher;
+    }
+    
+    /**
+     * This method return the course's ID.
+     * @return a String.
+     */
+    public String getId(){
+        return this.id;
+    }
+    
+    /**
+     * This method return the course's duration.
+     * @return an Integer.
+     */
+    public int getDuration(){
+        return this.duration;
     }
     
     /**
